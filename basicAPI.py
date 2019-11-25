a5496540c996760e140221f8d1d7fcb4ed49f4aa
saurabh <asu@test.com>
2019-11-25 13:02:29 +0530

    all done

--- basicAPI.py
+++ basicAPI.py
@@ -131,8 +131,8 @@ def create_user():
 @app.route('/user/<public_id>', methods=['PUT'])
 def promote_user(public_id):
 
-    if not current_user.admin:
-        return jsonify({'message' : 'Cannot perform that function!'})
+    # if not current_user.admin:
+    #     return jsonify({'message' : 'Cannot perform that function!'})
 
     user = User.query.filter_by(public_id=public_id).first()
 
@@ -164,7 +164,76 @@ def delete_user(current_user,public_id):
 
     return jsonify({'message' : 'The user has been deleted!'})
 
+##################TODO####################
+@app.route('/todo',methods=["GET"])
+@token_required
+def get_all_todos(current_user):
+    todos = Todo.query.filter_by(user_id=current_user.id).all()
 
+    output = []
+
+    for todo in todos:
+        todo_data = {}
+        todo_data['id'] = todo.id
+        todo_data['text'] = todo.text
+        todo_data['complete'] = todo.complete
+        output.append(todo_data)
+
+    return jsonify({'todos' : output})
+
+
+@app.route('/todo/<todo_id>',methods=["GET"])
+@token_required
+def get_one_todo(current_user,todo_id):
+
+    todo = Todo.query.filter_by(id=todo_id, user_id=current_user.id).first()
+
+    if not todo:
+        return jsonify({'message' : 'No todo found!'})
+
+    todo_data = {}
+    todo_data['id'] = todo.id
+    todo_data['text'] = todo.text
+    todo_data['complete'] = todo.complete
+
+    return jsonify(todo_data)    
+
+
+@app.route('/todo', methods=['POST'])
+@token_required
+def create_todo(current_user):
+    data = request.get_json()
+
+    new_todo = Todo(text=data['text'], complete=False, user_id=current_user.id)
+    db.session.add(new_todo)
+    db.session.commit()
+
+    return jsonify({'message' : "Todo created!"})
+
+@app.route('/todo/<todo_id>',methods=["PUT"])
+@token_required
+def complete_todo(current_user,todo_id):
+    todo = Todo.query.filter_by(id=todo_id, user_id=current_user.id).first()
+
+    if not todo:
+        return jsonify({'message' : 'No todo found!'})
+
+    todo.complete = True
+    db.session.commit()
+
+    return jsonify({'message' : 'Todo item has been completed!'})
+@app.route('/todo/<todo_id>', methods=["DELETE"])
+@token_required
+def delete_todo(current_user,todo_id):
+    todo = Todo.query.filter_by(id=todo_id, user_id=current_user.id).first()
+
+    if not todo:
+        return jsonify({'message' : 'No todo found!'})
+
+    db.session.delete(todo)
+    db.session.commit()
+
+    return jsonify({'message' : 'Todo item deleted!'})
 
 
 #################### 3rd step SETTING UP AUTHORIZATION using http basic authentcation ##################
